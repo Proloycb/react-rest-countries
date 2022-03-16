e45a9c9a448e192c30f9dd844590adde2380d033
Proloycb <proloychacroborty@gmail.com>
2022-03-17 00:33:02 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
+import Person from './components/Person/Person';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <Header></Header>
+      <Countries></Countries>
+      <Person></Person>
     </div>
   );
 }
 
-function LoadCountries() {
+/* function LoadCountries() {
   const [countries, setCountries] = useState([]);
   useEffect( () => {
     fetch('https://restcountries.com/v3.1/all')
@@ -35,5 +40,5 @@ function Country (props) {
       <h4>Population: {props.population}</h4>
     </div>
   )
-}
+} */
 export default App;
---
+++ src/components/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Person = () => {
+    return (
+        <div>
+            <h3>This is person component</h3>
+        </div>
+    );
+};
+
+export default Person;
