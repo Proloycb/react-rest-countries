74befb45196db48eeac2cca237460ee75ea956a6
Proloycb <proloychacroborty@gmail.com>
2022-03-17 11:48:26 +0600

    finished

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-dark p-5'>
+            <p className='text-white'>@ Copywrite protected</p>
+        </div>
+    );
+};
+
+export default Footer;
